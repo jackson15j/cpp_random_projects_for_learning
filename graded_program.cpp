bce1b7f6efdac49897aa94b7a5b58092ab0978d5
Craig Astill <craig.astill@gmail.com>
2015-12-30 14:38:13 +0000

    Dirty if statements to solve additional grades
    Would rather do this in a better way, but going quick and dirty first.

--- graded_program.cpp
+++ graded_program.cpp
@@ -21,10 +21,39 @@ using namespace std;
 int main()
 {
     int score;
-    cout << "Enter a score (0-100): " << endl;
+    cout << "Enter a score (0-100): ";
     cin >> score;
+    if ((score > 100) || (score < 0))
+    {
+        cout << score << ", is not between: 0-100." << endl;
+        return 1;
+    }
     if (score == 100)
     {
         cout << "You got a perfect score" << endl;
+    }
+    if ((score >= 90) && (score <= 100))
+    {
+        cout << "A score of: " << score << ", gets you an: A" << endl;
+    }
+    if ((score >= 80) && (score <= 89))
+    {
+        cout << "A score of: " << score << ", gets you a: B" << endl;
+    }
+    if ((score >= 70) && (score <= 79))
+    {
+        cout << "A score of: " << score << ", gets you a: C" << endl;
+    }
+    if ((score >= 60) && (score <= 69))
+    {
+        cout << "A score of: " << score << ", gets you a: D" << endl;
+    }
+    if ((score >= 50) && (score <= 59))
+    {
+        cout << "A score of: " << score << ", gets you an: F" << endl;
+    }
+    if (score < 50)
+    {
+        cout << "We don't give marks below 50." << endl;
     }
 }
