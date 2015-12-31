afd1ec52ed8a5a96fa8a539cef70df71c2b40e5e
Craig Astill <craig.astill@gmail.com>
2015-12-31 16:24:54 +0000

    Add sections to test for reporting.

--- graded_program/test/test_graded_program.cpp
+++ graded_program/test/test_graded_program.cpp
@@ -7,26 +7,50 @@
 TEST_CASE("Test get_grade function.")
 {
     REQUIRE(1 == 1);
-    int score = 100;
-    REQUIRE(get_grade(score) == "P");
-    REQUIRE(get_grade(score) != "X");
+    int score;
+    SECTION("Lets check perfect score of 100.")
+    {
+        score = 100;
+        REQUIRE(get_grade(score) == "P");
+        REQUIRE(get_grade(score) != "X");
+    }
 
-    for (int i=90; i < 100; i++)
-        REQUIRE(get_grade(i) == "A");
-    for (int i=80; i < 90; i++)
-        REQUIRE(get_grade(i) == "B");
-    for (int i=70; i < 80; i++)
-        REQUIRE(get_grade(i) == "C");
-    for (int i=60; i < 70; i++)
-        REQUIRE(get_grade(i) == "D");
-    for (int i=50; i < 60; i++)
-        REQUIRE(get_grade(i) == "F");
-    for (int i=0; i < 50; i++)
-        REQUIRE(get_grade(i) == "U");
+    SECTION("Lets check: A, grades (90-100).")
+    {
+        for (int i=90; i < 100; i++)
+            REQUIRE(get_grade(i) == "A");
+    }
+    SECTION("Lets check: A, grades (90-100).")
+    {
+        for (int i=80; i < 90; i++)
+            REQUIRE(get_grade(i) == "B");
+    }
+    SECTION("Lets check: A, grades (90-100).")
+    {
+        for (int i=70; i < 80; i++)
+            REQUIRE(get_grade(i) == "C");
+    }
+    SECTION("Lets check: A, grades (90-100).")
+    {
+        for (int i=60; i < 70; i++)
+            REQUIRE(get_grade(i) == "D");
+    }
+    SECTION("Lets check: A, grades (90-100).")
+    {
+        for (int i=50; i < 60; i++)
+            REQUIRE(get_grade(i) == "F");
+    }
+    SECTION("Lets check: A, grades (90-100).")
+    {
+        for (int i=0; i < 50; i++)
+            REQUIRE(get_grade(i) == "U");
+    }
 
-    // out of bounds.
-    score = 101;
-    REQUIRE(get_grade(score) == "X");
-    score = -1;
-    REQUIRE(get_grade(score) == "X");
+    SECTION("Now lets check out of bounds (<0, >100).")
+    {
+        score = 101;
+        REQUIRE(get_grade(score) == "X");
+        score = -1;
+        REQUIRE(get_grade(score) == "X");
+    }
 }
