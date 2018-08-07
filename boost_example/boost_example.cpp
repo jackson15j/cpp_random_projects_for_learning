1857d1716faea0041a9002ee571484ffc2d758ec
Craig Astill <craig.astill@gmail.com>
2018-08-07 16:42:29 +0100

    Tried a quick boost_example after package install on Arch.

---
+++ boost_example/boost_example.cpp
@@ -0,0 +1,29 @@
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+
+/**
+   Boost Example.
+
+   * https://www.boost.org/doc/libs/1_67_0/more/getting_started/unix-variants.html
+
+   * Compile via: `c++ -I <path/to/boost> boost_example.cpp -o boost_example`.
+   * Run via: `echo 1 2 3 | ./boost_example`.
+
+   Since I used pacman to install Boost, all of it's libraries are in
+   `/usr/lib` (https://www.archlinux.org/packages/extra/x86_64/boost-libs/),
+   and files are in `/usr/include/boost`
+   (https://www.archlinux.org/packages/extra/x86_64/boost/) so my compile
+   string was:
+
+   `c++ boost_example.cpp -o boost_example && echo 1 2 3 | ./boost_example`
+ */
+int main() {
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
